6eefb2eb97cc50eb29e03c3a54e90967dfea6697
zuckeyM-17 <kazuki.matsumura@studist.jp>
2023-05-14 23:15:21 +0900

    read from env

--- src/setup_daily_jobs.go
+++ src/setup_daily_jobs.go
@@ -1,8 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-
 	fmt.Println("Hello World")
+	fmt.Println("NOTION_API_TOKEN: ", os.Getenv("NOTION_API_TOKEN"))
+	fmt.Println("DAILY_TASK_DATABASE_ID: ", os.Getenv("DAILY_TASK_DATABASE_ID"))
 }
