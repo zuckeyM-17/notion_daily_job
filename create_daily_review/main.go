056a16fdc311ca29d4b28b0476eb45e8e14b7623
zuckeyM-17 <kazuki.matsumura@studist.jp>
2023-06-20 23:48:11 +0900

    add daily review content

--- create_daily_review/main.go
+++ create_daily_review/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"strings"
 	"time"
@@ -37,10 +36,6 @@ func main() {
 	query = strings.Replace(query, "DONE_DATE", today, 1)
 	tasks, _ := notion.GetTasks(notionToken, databaseId, query)
 
-	for _, task := range tasks {
-		fmt.Println(task.Properties.Name.Title[0].PlainText)
-	}
-
 	propertiesTemplate := `{
 		"parent": { "database_id": "DATABASE_ID" },
 		"properties": {
@@ -60,26 +55,39 @@ func main() {
 			}
 		},
 		"children":	[
-			{
-				"object": "block",
-				"type": "paragraph",
-				"paragraph": {
-					"rich_text": [
-						{
-							"type": "text",
-							"text": {
-								"content": "DONE_TASKS"
-							}
-						}
-					]
-				}
-			}
+			CHILDREN
 		]
 	}`
+
+	content := `{
+		"object": "block",
+		"type": "paragraph",
+		"paragraph": {
+			"rich_text": [
+				{
+					"type": "text",
+					"text": {
+						"content": "TEXT"
+					}
+				}
+			]
+		}
+	}`
+
+	var contents string
+
+	for i, task := range tasks {
+		c := strings.Replace(content, "TEXT", task.Properties.Name.Title[0].PlainText, 1)
+		contents = contents + c
+		if i != len(tasks)-1 {
+			contents = contents + ","
+		}
+	}
 
 	properties := strings.Replace(propertiesTemplate, "DATABASE_ID", databaseId, 1)
 	properties = strings.Replace(properties, "START_DATE", today, 1)
 	properties = strings.Replace(properties, "TITLE", "["+today+"] 日次振り返り", 1)
+	properties = strings.Replace(properties, "CHILDREN", contents, 1)
 
 	notion.CreateTask(notionToken, properties)
 }
